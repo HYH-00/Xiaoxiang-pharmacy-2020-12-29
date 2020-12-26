092a1dd996ecbb6fe7550961e4ef301b62691640
李亚当 <2501228993@qq.com>
2020-12-26 14:37:05 +0800

    用户数据可视化分析以及词云模拟分析

--- back/src/main/java/com/example/demo/Controller/UserInformationController.java
+++ back/src/main/java/com/example/demo/Controller/UserInformationController.java
@@ -1,5 +1,6 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Bean.GoodsTypeNumber;
 import com.example.demo.Bean.User;
 import com.example.demo.Service.UserInformationService;
 import org.apache.ibatis.annotations.Param;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -18,6 +20,11 @@ public class UserInformationController {
     @Autowired
     private UserInformationService userInformationService;
 
+    /**
+     * 验证用户身份
+     * @param user 用户信息
+     * @return 返回结果
+     */
     @RequestMapping("/verification")
     @ResponseBody
     public Map<String,Boolean> verificationUserController(@RequestBody User user){
@@ -25,5 +32,25 @@ public class UserInformationController {
         Map<String,Boolean> map = new HashMap<>();
         map.put("flag",flag);
         return map;
+    }
+
+    /**
+     * 用户金额数据分析数据
+     * @param userid 用户id
+     * @return 返回数据分析结果集
+     */
+    @ResponseBody
+    @RequestMapping("/userDataAnalysis")
+    public List<GoodsTypeNumber> userDataAnalysis(@RequestParam("userid") String userid){
+        return userInformationService.userDataAnalysisService(userid);
+    }
+
+    /**
+     * 用户余额
+     * @param userid 用户id
+     * @return 返回余额
+     */
+    public double userBalanceController(@RequestParam("userid") String userid){
+        return userInformationService.userBalanceService(userid);
     }
 }
--- back/src/main/java/com/example/demo/Dao/UserInformationDao.java
+++ back/src/main/java/com/example/demo/Dao/UserInformationDao.java
@@ -1,8 +1,11 @@
 package com.example.demo.Dao;
 
+import com.example.demo.Bean.GoodsTypeNumber;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface UserInformationDao {
@@ -14,4 +17,20 @@ public interface UserInformationDao {
      */
     @Select("select count(*) from user where name = #{name} and password = #{password}")
     public int verificationUser(@Param("name") String name,@Param("password") String password);
+
+    /**
+     * 用户订单类型金额数据分析
+     * @param userid 用户id
+     * @return 返回数据集
+     */
+    @Select("select goods.type as type,COUNT(*) as number,SUM(order1.allprice) as amount,SUM(order1.discount) as discount from order1,goods where order1.userid = #{userid} and order1.shopid = goods.shopid GROUP BY goods.type ;")
+    public List<GoodsTypeNumber> GoodsTypeDataAnalysis(String userid);
+
+    /**
+     * 用户余额查询
+     * @param userid 用户id
+     * @return 返回余额
+     */
+    @Select("select balance from user where name = #{userid}")
+    public double userBalance(String userid);
 }
